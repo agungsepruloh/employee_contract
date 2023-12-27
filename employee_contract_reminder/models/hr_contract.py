75be9944d16f505b78a2aad45aacb0fd9ccb1875
Agung Sepruloh <agungsepruloh1996@gmail.com>
2023-12-27 11:53:48 +0700

    [IMP] hr.contract: Do some refactoring and update manifest version

--- employee_contract_reminder/models/hr_contract.py
+++ employee_contract_reminder/models/hr_contract.py
@@ -6,18 +6,23 @@ class HrContract(models.Model):
 
     @api.model
     def _cron_employee_contract_reminder(self):
-        contract_ids = self.search([('state', '=', 'open')])
         contract_reminder_days = self.env['ir.config_parameter'].sudo().get_param('employee_contract_reminder.contract_reminder_days')
         if not contract_reminder_days:
-            pass
+            return False
+        result = []
+        contract_ids = self.search([('state', '=', 'open'), ('date_end', '!=', False)])
         for contract in contract_ids:
-            if contract.date_end:
-                date_end = fields.Date.from_string(contract.date_end)
-                today = fields.Date.from_string(fields.Date.today())
-                if (date_end - today).days <= int(contract_reminder_days):
-                    template = self.env.ref('employee_contract_reminder.email_template_employee_contract_reminder')
-                    template.send_mail(contract.id, force_send=True)
-                    if contract.employee_id.work_email:
-                        template.send_mail(contract.id, force_send=True,
-                                           email_values={'email_to': contract.employee_id.work_email})
-        return True
+            date_end = fields.Date.from_string(contract.date_end)
+            today = fields.Date.from_string(fields.Date.today())
+            if (date_end - today).days <= int(contract_reminder_days) and contract.employee_id.work_email:
+                contract.send_mail_reminder()
+                result.append(contract)
+        return result
+
+    def send_mail_reminder(self):
+        """
+        Send email to employee
+        :return:
+        """
+        template = self.env.ref('employee_contract_reminder.email_template_employee_contract_reminder')
+        template.send_mail(self.id, force_send=True, email_values={'email_to': self.employee_id.work_email})
