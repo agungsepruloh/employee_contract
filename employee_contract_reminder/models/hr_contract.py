7d0aa01a546bd91476cca4cb25ca3bec19795aa6
Agung Sepruloh <agungsepruloh1996@gmail.com>
2023-12-31 17:09:11 +0700

    [IMP] hr.contract: Separate logic between get contracts to remind and send the mail reminder

--- employee_contract_reminder/models/hr_contract.py
+++ employee_contract_reminder/models/hr_contract.py
@@ -1,3 +1,4 @@
+from dateutil.relativedelta import relativedelta
 from odoo import api, models, fields, _
 
 
@@ -6,18 +7,31 @@ class HrContract(models.Model):
 
     @api.model
     def _cron_employee_contract_reminder(self):
-        contract_reminder_days = self.env['ir.config_parameter'].sudo().get_param('employee_contract_reminder.contract_reminder_days')
+        """
+        Cron job to send email to employee
+        :return:
+        """
+        contract_ids = self.get_contracts_to_remind().filtered(lambda contract: contract.employee_id.work_email)
+        contract_ids.send_mail_reminder()
+        return contract_ids
+
+    @api.model
+    def get_contracts_to_remind(self):
+        """
+        Get contracts to remind
+        :return:
+        """
+        contract_reminder_days = self.env['ir.config_parameter'].sudo().get_param(
+            'employee_contract_reminder.contract_reminder_days')
         if not contract_reminder_days:
-            return False
-        result = []
-        contract_ids = self.search([('state', '=', 'open'), ('date_end', '!=', False)])
-        for contract in contract_ids:
-            date_end = fields.Date.from_string(contract.date_end)
-            today = fields.Date.from_string(fields.Date.today())
-            if (date_end - today).days <= int(contract_reminder_days) and contract.employee_id.work_email:
-                contract.send_mail_reminder()
-                result.append(contract)
-        return result
+            return []
+        contract_ids = self.search([
+            ('state', '=', 'open'),
+            ('date_end', '!=', False),
+            ('date_end', '>=', fields.Date.today()),
+            ('date_end', '<=', fields.Date.today() + relativedelta(days=int(contract_reminder_days)))
+        ])
+        return contract_ids
 
     def send_mail_reminder(self):
         """
@@ -25,4 +39,5 @@ class HrContract(models.Model):
         :return:
         """
         template = self.env.ref('employee_contract_reminder.email_template_employee_contract_reminder')
-        template.send_mail(self.id, force_send=True, email_values={'email_to': self.employee_id.work_email})
+        for contract in self:
+            template.send_mail(contract.id, force_send=True, email_values={'email_to': contract.employee_id.work_email})
