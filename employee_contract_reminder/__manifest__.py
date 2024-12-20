b55c8ab8ffdac634dbd049e8260a6eb83a57d1de
Agung Sepruloh <agungsepruloh1996@gmail.com>
2024-12-20 23:46:53 +0700

    [IMP] hr.contract: Enable for Odoo 18

--- employee_contract_reminder/__manifest__.py
+++ employee_contract_reminder/__manifest__.py
@@ -19,17 +19,14 @@
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Generic Modules/Human Resources',
-    'version': '17.0.1.0.1',
+    'version': '18.0.1.0.1',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'hr_contract'],
 
     # always loaded
     'data': [
-        # Views
         'views/res_config_settings_views.xml',
-
-        # Data
         'data/ir_cron_data.xml',
         'data/mail_data.xml',
     ],
